9c4a970bc227f26cf6baa86f163b17aaa6852624
edinkoc <99792461+edinkoc@users.noreply.github.com>
2025-03-02 13:41:51 +0300

    - New Route of the site has been updated

--- src/routes/AppRoute.jsx
+++ src/routes/AppRoute.jsx
@@ -5,24 +5,44 @@ import SignUp from "../pages/authentication/signup/SignUp"
 import CustomerIndex from "../pages/customer/CustomerIndex"
 import HomePage from "../pages/customer/HomePage"
 import About from "../pages/customer/About"
-import ContactUs from "../pages/customer/ContactUs"
 import Brands from "../pages/customer/Brands"
+import Chatbot from "../Chatbot/Chatbot"
+import AdminIndex from "../pages/admin/AdminIndex"
+import Medicine from "../pages/admin/Medicine/Medicine"
+import Customers from "../pages/admin/Customers/Customers"
+import OrderDetails from "../pages/admin/Orders/OrderDetails"
+import ContactUs from "../pages/customer/ContactUs"
 import News from "../pages/customer/News"
 import OperationsAndPolicies from "../pages/customer/OperationsAndPolicies"
-import Chatbot from "../Chatbot/Chatbot"
+import Body from "../components/admin/Body/Body"
+import InventoryLevel from "../pages/admin/InventoryLevel/InventoryLevel"
+
 
 const AppRoutes = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" Component={CustomerIndex}>
-                    <Route path="/" Component={HomePage} />
-                    <Route path="/customer/About" Component={About} />
-                    <Route path="/customer/ContactUs" Component={ContactUs} />
-                    <Route path="/customer/Brands" Component={Brands} />
-                    <Route path="/customer/News" Component={News} />
-                    <Route path="/customer/OperationsAndPolicies" Component={OperationsAndPolicies} />
-                    <Route path="/Chatbot/chatbot" Component={Chatbot} />
+                <Route path="/auth" element={<AuthenticationIndex />}>
+                    <Route path="/auth/login" element={<Login />} />
+                    <Route path="/auth/signup" element={<SignUp />} />
+                </Route>
+                
+                <Route path="/" element={<CustomerIndex />}>
+                    <Route index element={<HomePage />} />
+                    <Route path="customer/about" element={<About />} />
+                    <Route path="customer/brands" element={<Brands />} />
+                    <Route path="customer/chatbot" element={<Chatbot />} />
+                    <Route path="customer/contactus" element={<ContactUs />} />
+                    <Route path="customer/news" element={<News />} />
+                    <Route path="customer/operationsandpolicies" element={<OperationsAndPolicies />} />
+                </Route>
+
+                <Route path="/admin" element={<AdminIndex />}>
+                    <Route path="/admin/" element={<Body />} />
+                    <Route path="/admin/medicine" element={<Medicine />} />
+                    <Route path="/admin/inventory" element={<InventoryLevel />} />
+                    <Route path="/admin/customers" element={<Customers />} />
+                    <Route path="/admin/orders" element={<OrderDetails />} />
                 </Route>
             </Routes>
         </BrowserRouter>
@@ -30,12 +50,3 @@ const AppRoutes = () => {
 }
 
 export default AppRoutes
-
-/*
-                <Route path="/" Component={AuthenticationIndex}>
-                    <Route path="/" Component={Login} />
-                    <Route path="/login" Component={Login} />
-                    <Route path="/signup" Component={SignUp} />
-                </Route>
-
-*/
